94d81d9f66dcf5871adb30f5a1bffe3646f23a78
Unknown <giovannifdias@gmail.com>
2018-12-03 23:26:21 -0200

    atualizando alguns dados e controllers

--- api/controllers/FuncionarioController.js
+++ api/controllers/FuncionarioController.js
@@ -6,7 +6,35 @@
  */
 
 module.exports = {
-  
-
+  get: (req, res) => {
+    Funcionario.find({
+      id: req.param("id")
+    }).then(users => {
+      if (users.length > 0) {
+        res.json(users[0]);
+      } else {
+        res.badRequest({
+          err: "not found"
+        });
+      }
+    });
+  },
+  auth: (req, res) => {
+    sails.log.info("json", req.allParams());
+    Funcionario.findOne({
+      email: req.param("email"),
+      password: req.param("password"),
+      status: true
+    })
+      .then(employee => {
+        if (!employee) {
+          employee = "Não encontrado";
+        }
+        delete employee.password;
+        res.json(employee);
+      })
+      .catch(err => {
+        res.badRequest(err);
+      });
+  }
 };
-
--- api/models/Funcionario.js
+++ api/models/Funcionario.js
@@ -18,6 +18,14 @@ module.exports = {
       required: true,
       unique: true
     },
+    email:{
+      type: 'string',
+      required: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    },
     nome: {
       type: 'string',
       required: true
